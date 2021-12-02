7cb1323285737af7f4fad7ca7793d1938a0ef95b
Karthikeya2001 <57314420+Karthikeya2001@users.noreply.github.com>
2021-12-02 22:41:38 +0530

    Update ibm.py

--- Food demand forecast project/flask/ibm.py
+++ Food demand forecast project/flask/ibm.py
@@ -1,7 +1,7 @@
 import requests
 
 # NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-API_KEY = "HdW0sTUJV2dQyaLB_voIUYGOwWkXZAMSiEmoYzZEaqOu"
+API_KEY = "KJ0ACMzGlyHWKZcxa-PjT5RJPpghNmXqAOCYI_kYVfKH"
 token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
 mltoken = token_response.json()["access_token"]
 
@@ -13,7 +13,7 @@ payload_scoring = {"input_data":
        'city_code', 'region_code', 'category']], 
 "values": [[0.,0.,3.,1.,647.,56.,11.]]}]}
 
-response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/fceca4bb-5665-47f6-bb69-0d91eb60e1b4/predictions?version=2021-11-17', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+response_scoring = requests.post('https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/12bec9a4-3c6e-4775-b55b-7990da812138/predictions?version=2021-12-02', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
 print("Scoring response")
 print(response_scoring.json())
 predictions =response_scoring.json()
